86d0f26b26322bb8838e8e7bddf8780d6beac894
Jorge Peniche <jorge.peniche@MacBookPro-JP.local>
2026-06-11 16:18:36 -0600

    Task 6: updated function

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,7 @@
 for i in range(10):
     for j in range(10):
         if i < j:
-            print("{}{}".format(i, j))
+            print(
+                "" if i == 0 and j == 1 else ", ",
+                "{}{}".format(i, j), sep="", end=""
+                )
