bf2043627be60079229f5df0ae14c8a115e12a49
Jorge Peniche <jorge.peniche@MacBookPro-JP.local>
2026-06-11 16:49:03 -0600

    Task 8: 1st commit

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str_in):
+    result = ""
+    for i in str(str_in):
+        if 97 <= ord(i) <= 122:
+            result += chr(ord(i)-32)
+        else:
+            result += str(i)
+    print(result)
