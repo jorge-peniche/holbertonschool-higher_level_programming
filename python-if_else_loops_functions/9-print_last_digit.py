a2d8e858514067063134ad72c34eba104b916f9d
Jorge Peniche <jorge.peniche@MacBookPro-JP.local>
2026-06-11 17:16:37 -0600

    Task 9: Tried implementing definition and acceptance criteria

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    return int(str(number)[-1])
+    last_num = str(number)[-1]
+    print(f"{last_num}", end="")
+    return last_num
+
+
+print_last_digit(98)
+print_last_digit(0)
+r = print_last_digit(-1024)
+print(r)
