8946a7e357910cee8e4d502a5b9240e8b6bcd9b5
Jorge Peniche <jorge.peniche@MacBookPro-JP.local>
2026-06-11 16:56:05 -0600

    Task 8: Skipped forcing input into string

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str_in):
     result = ""
-    for i in str(str_in):
+    for i in str_in:
         if 97 <= ord(i) <= 122:
             result += chr(ord(i)-32)
         else:
