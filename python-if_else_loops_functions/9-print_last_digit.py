e18a926f66a8ecaf39f7790fc35583e1de454f6b
Jorge Peniche <jorge.peniche@MacBookPro-JP.local>
2026-06-11 17:20:18 -0600

    Task9: Checks that is int

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    last_num = str(number)[-1]
+    if isinstance(number, int):
+        last_num = str(number)[-1]
     print(f"{last_num}", end="")
     return last_num
